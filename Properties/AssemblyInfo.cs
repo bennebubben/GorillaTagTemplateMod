0fd767f8395a2fdd1ead5fc8576b3e22bdf63053
bennebubben <134714554+bennebubben@users.noreply.github.com>
2026-08-08 23:14:20 +0200

    Update assembly title and product name

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("modtemp")]
+[assembly: AssemblyTitle("TemplateMod")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("modtemp")]
+[assembly: AssemblyProduct("TemplateMod")]
 [assembly: AssemblyCopyright("Copyright ©  2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
